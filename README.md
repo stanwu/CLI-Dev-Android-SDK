6bd3b296217ab740997dbce4ad3a973ee56b0919
Stan Ht. Wu <stanwu.tw@gmail.com>
2023-12-16 14:31:07 +0000

    snapshot

--- README.md
+++ README.md
@@ -3,31 +3,29 @@
 Template for Android Development Environment (for CLI developer)
 You don't need to install any GUI or Desktop environment for your Android app development environment. 
 
-**This template currently does not support Codespace, but it supports Gitpod and Google Cloud Platform.**
-
 ### How to use this template?
 
 You just need to copy **install_android-sdk.sh** and **install_android-sdk.bashrc**, these two files, into your Android App Project's source code. It's simple!
 
 ```sh
 sudo ./install_android-sdk.sh
-cat ./install_android-sdk.bashrc >> ~/.bashrc
 cd /opt
 sudo chown -R $USER android gradle gradlew
 ```
 
-Test commands to make sure works
+Add below to .bashrc or .zshrc
 
 ```sh
-cd ~/
-gradle
-gradlew
+export ANDROID_HOME="/opt/android"
+export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
+export PATH="$PATH:$JAVA_HOME/bin:$ANDROID_HOME/emulator:$ANDROID_HOME/cmdline-tools/tools/bin:$ANDROID_HOME/platform-tools:${ANDROID_NDK_HOME}"
+export LD_LIBRARY_PATH="$ANDROID_HOME/emulator/lib64:$ANDROID_HOME/emulator/lib64/qt/lib"
 ```
 
 Try to build Hello World to make sure development environment is working now
 
 ```sh
-git clone https://github.com/francohorita/android-hello-world.git
+git clone https://github.com/chiuki/android-hello-world.git
 cd android-hello-world
 gradlew tasks
 gradlew assembledebug
@@ -44,5 +42,5 @@ For GitHub Codepsace Java issue
 
 ```sh
 cd /home/codespace/java
-rm current && ln -sf /home/codespace/java/17.0.9-ms current
+rm current && ln -sf /usr/lib/jvm/java-11-openjdk-amd64 current
 ```
--- install_android-sdk.sh
+++ install_android-sdk.sh
@@ -16,26 +16,14 @@ if [ $UID -gt 0 ]; then
 	exit 1
 fi
 
-sudo apt update -qq && apt install -qq -y unzip openjdk-8-jdk vim git unzip libglu1 libpulse-dev libasound2 libc6  libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxi6  libxtst6 libnss3 wget
+apt update -qq && apt install -qq -y unzip openjdk-11-jdk vim git unzip libglu1 libpulse-dev libasound2 libc6  libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxi6  libxtst6 libnss3 wget
 
-sudo wget -q https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip -P /tmp \
-&& unzip -q -d /opt/gradle /tmp/gradle-${GRADLE_VERSION}-bin.zip \
-&& mkdir /opt/gradlew \
-&& /opt/gradle/gradle-${GRADLE_VERSION}/bin/gradle wrapper --gradle-version ${GRADLE_VERSION} --distribution-type all -p /opt/gradlew  \
-&& /opt/gradle/gradle-${GRADLE_VERSION}/bin/gradle wrapper -p /opt/gradlew
-
-sudo mkdir /opt/android \
+mkdir /opt/android \
 && mkdir /opt/android/cmdline-tools \
 && wget -q 'https://dl.google.com/android/repository/commandlinetools-linux-6200805_latest.zip' -P /tmp \
 && unzip -q -d /opt/android/cmdline-tools /tmp/commandlinetools-linux-6200805_latest.zip \
 && yes Y | /opt/android/cmdline-tools/tools/bin/sdkmanager --install "build-tools;${ANDROID_BUILD_TOOLS_LEVEL}" "platforms;android-${ANDROID_API_LEVEL}" "platform-tools" "ndk;${ANDROID_NDK_VERSION}" \
 && yes Y | /opt/android/cmdline-tools/tools/bin/sdkmanager --licenses
-
-export GRADLE_HOME=/opt/gradle/gradle-$GRADLE_VERSION
-export ANDROID_HOME=/opt/android
-export ANDROID_NDK_HOME=${ANDROID_HOME}/ndk/${ANDROID_NDK_VERSION}
-export PATH="$PATH:$GRADLE_HOME/bin:/opt/gradlew:$ANDROID_HOME/emulator:$ANDROID_HOME/cmdline-tools/tools/bin:$ANDROID_HOME/platform-tools:${ANDROID_NDK_HOME}"
-export LD_LIBRARY_PATH="$ANDROID_HOME/emulator/lib64:$ANDROID_HOME/emulator/lib64/qt/lib"
 
 echo "This template is compatible with Java versions below 17.0.9."
 javac -version
